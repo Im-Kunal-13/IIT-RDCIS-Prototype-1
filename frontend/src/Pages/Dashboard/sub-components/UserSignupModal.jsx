ddf05aca4c99bbe056366ce163d11c4aa6313e21
ImKunal13z <2002kunalmondal13@gmail.com>
2022-04-25 17:27:35 +0530

    now

--- frontend/src/Pages/Dashboard/sub-components/UserSignupModal.jsx
+++ frontend/src/Pages/Dashboard/sub-components/UserSignupModal.jsx
@@ -1,18 +1,15 @@
+import { handle } from "express/lib/application";
 import React, { useState } from "react";
 
 // INITIAL STATE OF THE FORM.
 const initialState = {
-  fullName: "",
-  username: "",
+  name: "",
   email: "",
-  college: "",
-  role: "admin",
-  enrollmentNo: "",
-  passoutYear: "",
+  phone: "",
+  organization: "",
+  administrator: false,
   password: "",
   confirmPassword: "",
-  phoneNumber: "",
-  avatarURL: "",
 };
 
 export default function UserSignupModal() {
@@ -30,12 +27,12 @@ export default function UserSignupModal() {
   // FUNCTION WHICH WILL BE TRIGERED ON FORM DATA CHANGE.
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
-    // console.log(form);
   };
 
   // SUBMIT BUTTON HANDLE FUNCTION
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log(form);
   };
 
   return (
@@ -71,211 +68,233 @@ export default function UserSignupModal() {
               </h5>
             </div>
           </div>
-          {/* NAME  */}
           <div className="modal-body">
-            {/* NAME  */}
-            <div className="input-group mb-4">
-              <span
-                className="input-group-text form-labels border-r-0 rounded-lg border-0 shadow"
-                id="basic-addon1"
-              >
-                <i className="bi bi-person-fill text-2xl text-white"></i>
-              </span>
-              <input
-                type="text"
-                className="form-control border-l-0 rounded-lg border-0 shadow"
-                placeholder="Name"
-                aria-label="Username"
-                value={form.fullName}
-                name="fullName"
-                onChange={handleChange}
-                aria-describedby="basic-addon1"
-              />
-            </div>
-            {/* EMAIL  */}
-            <div className="input-group my-4">
-              <span
-                className="input-group-text form-labels border-r-0 rounded-lg border-0 shadow"
-                id="basic-addon1"
-              >
-                <i className="bi bi-envelope-open-fill text-2xl text-white"></i>
-              </span>
-              <input
-                type="email"
-                className="form-control border-l-0 rounded-lg border-0 shadow"
-                placeholder="Email"
-                aria-label="Username"
-                value={form.email}
-                name="email"
-                onChange={handleChange}
-                aria-describedby="basic-addon1"
-              />
-            </div>
-            {/* PHONE  */}
-            <div className="input-group my-4">
-              <span
-                className="input-group-text form-labels border-r-0 rounded-lg border-0 shadow"
-                id="basic-addon1"
-              >
-                <i className="bi bi-telephone-fill text-2xl text-white"></i>
-              </span>
-              <input
-                type="number"
-                className="form-control border-l-0 rounded-lg border-0 shadow"
-                placeholder="Phone Number"
-                aria-label="Username"
-                value={form.phoneNumber}
-                name="phoneNumber"
-                onChange={handleChange}
-                aria-describedby="basic-addon1"
-              />
-            </div>
-            {/* ORGANIZATION */}
-            <div className="input-group my-4">
-              <span
-                className="input-group-text bg-themeViolet1 form-labels border-r-0 rounded-lg border-0 shadow"
-                id="basic-addon1"
-              >
-                <i className="bi bi-bank2 text-2xl text-white"></i>
-              </span>
-              <select
-                className="form-control border-l-0 rounded-lg border-0 shadow"
-                id="organization"
-              >
-                <option>Steel Authority of India</option>
-              </select>
-            </div>
-            {/* ADMIN OR NOT  */}
-            <div className="input-group my-4">
-              <span
-                className="input-group-text form-labels border-r-0 rounded-lg border-0 shadow"
-                id="basic-addon1"
-              >
-                <i className="bi bi-person-workspace text-2xl text-white"></i>
-              </span>
-              <input
-                type="text"
-                className={`form-control border-l-0 rounded-lg border-0 shadow text-${isAdmnin ? "green-600" : "red-600"} font-semibold`}
-                placeholder="Admin"
-                aria-label="Username"
-                value={isAdmnin ? "True" : "False"}
-                name="phoneNumber"
-                onChange={handleChange}
-                aria-describedby="basic-addon1"
-              />
-              <span
-                className="input-group-text form-labels border-l-0 rounded-lg border-0 shadow"
-                onClick={() => {
-                  setIsAdmin(!isAdmnin);
-                }}
-              >
-                <i
-                  className={`bi ${
-                    isAdmnin ? "bi-check-lg" : "bi-x-lg"
-                  } text-lg text-white`}
-                ></i>
-              </span>
-            </div>
-            {/* PASSWORD.  */}
-            <div className="input-group my-4">
-              <span
-                className="input-group-text form-labels border-r-0 rounded-lg border-0 shadow"
-                id="basic-addon1"
-              >
-                <i className="bi bi-key-fill text-xl text-white"></i>
-              </span>
-              <input
-                type={passwordVisibility ? "text" : "password"}
-                className="form-control border-l-0 rounded-lg border-0 shadow"
-                placeholder="Password"
-                aria-label="Username"
-                aria-describedby="basic-addon1"
-                onChange={handleChange}
-                value={form.password}
-                name="password"
-              />
-              <span
-                className="input-group-text form-labels border-l-0 rounded-lg border-0 shadow"
-                onClick={() => {
-                  setPasswordVisibility(!passwordVisibility);
-                }}
-              >
-                <i
-                  className={`bi ${
-                    passwordVisibility ? "bi-eye-slash" : "bi-eye-fill"
-                  } text-lg text-white`}
-                ></i>
-              </span>
-            </div>
-            {/* CONFIRM PASSWORD.  */}
-            <div className="input-group my-4">
-              <span
-                className="input-group-text form-labels border-r-0 rounded-lg border-0 shadow"
-                id="basic-addon1"
-              >
-                <i className="bi bi-key-fill text-2xl text-white"></i>
-              </span>
-              <input
-                type={confirmPasswordVisibility ? "text" : "password"}
-                className="form-control border-l-0 rounded-lg border-0 shadow"
-                placeholder="Confirm Password"
-                aria-label="Username"
-                aria-describedby="basic-addon1"
-                name="confirmPassword"
-                onChange={handleChange}
-                value={form.confirmPassword}
-              />
-              <span
-                className="input-group-text form-labels border-l-0 rounded-lg border-0 shadow"
-                onClick={() => {
-                  setConfirmPasswordVisibility(!confirmPasswordVisibility);
-                }}
-              >
-                <i
-                  className={`bi ${
-                    confirmPasswordVisibility ? "bi-eye-slash" : "bi-eye-fill"
-                  } text-lg text-white`}
-                ></i>
-              </span>
-            </div>
-            {/* TERMS & CONDITIONS  */}
-            <div className="form-check flex justify-center items-center mt-4">
-              <input
-                className="form-check-input mr-2 terms-checkbox"
-                type="checkbox"
-                value=""
-                id="flexCheckDefault"
-              />
-              <label
-                className="form-check-label text-xs mt-1"
-                htmlFor="flexCheckDefault"
-              >
-                I have read and agreed to{" "}
-                <span className="text-themeViolet1 cursor-pointer">
-                  Terms & Conditions
+            <form onSubmit={handleSubmit}>
+              {/* NAME  */}
+              <div className="input-group mb-4">
+                <span
+                  className="input-group-text form-labels border-r-0 rounded-lg border-0 shadow"
+                  id="basic-addon1"
+                >
+                  <i className="bi bi-person-fill text-2xl text-white"></i>
                 </span>
-              </label>
-            </div>
-            {/* SIGN UP BUTTON  */}
-            <div className="flex justify-center mt-4">
-              <button
-                className="rounded-lg py-2 px-8 landing-review text-white form-labels hover:scale-x-110 transition-all"
-                type="submit"
-                onClick={handleSubmit}
-              >
-                CREATE ACCOUNT
-              </button>
-            </div>
-            {/* ALREADY HAVE AN ACCOUNT  */}
-            <div className="form-check flex justify-center items-center my-4 p-0">
-              <label
-                className="form-check-label text-xs mt-1"
-                htmlFor="flexCheckDefault"
-              >
-                Already have an account?{" "}
-                <span className="text-lightBlue1 cursor-pointer">Sign In</span>
-              </label>
-            </div>
+                <input
+                  type="text"
+                  className="form-control border-l-0 rounded-lg border-0 shadow"
+                  placeholder="Name"
+                  aria-label="Username"
+                  value={form.name}
+                  name="name"
+                  onChange={handleChange}
+                  aria-describedby="basic-addon1"
+                  required
+                />
+              </div>
+              {/* EMAIL  */}
+              <div className="input-group my-4">
+                <span
+                  className="input-group-text form-labels border-r-0 rounded-lg border-0 shadow"
+                  id="basic-addon1"
+                >
+                  <i className="bi bi-envelope-open-fill text-2xl text-white"></i>
+                </span>
+                <input
+                  type="email"
+                  className="form-control border-l-0 rounded-lg border-0 shadow"
+                  placeholder="Email"
+                  aria-label="Username"
+                  value={form.email}
+                  name="email"
+                  onChange={handleChange}
+                  aria-describedby="basic-addon1"
+                  required
+                />
+              </div>
+              {/* PHONE  */}
+              <div className="input-group my-4">
+                <span
+                  className="input-group-text form-labels border-r-0 rounded-lg border-0 shadow"
+                  id="basic-addon1"
+                >
+                  <i className="bi bi-telephone-fill text-2xl text-white"></i>
+                </span>
+                <input
+                  type="number"
+                  className="form-control border-l-0 rounded-lg border-0 shadow"
+                  placeholder="Phone Number"
+                  aria-label="Username"
+                  value={form.phone}
+                  name="phone"
+                  onChange={handleChange}
+                  aria-describedby="basic-addon1"
+                  required
+                />
+              </div>
+              {/* ORGANIZATION */}
+              <div className="input-group my-4">
+                <span
+                  className="input-group-text bg-themeViolet1 form-labels border-r-0 rounded-lg border-0 shadow"
+                  id="basic-addon1"
+                >
+                  <i className="bi bi-bank2 text-2xl text-white"></i>
+                </span>
+                <select
+                  className="form-control border-l-0 rounded-lg border-0 shadow"
+                  value={form.organization}
+                  name="organization"
+                  onChange={handleChange}
+                  required
+                >
+                  <option>Steel Authority of India</option>
+                </select>
+              </div>
+              {/* ADMIN OR NOT  */}
+              <div className="input-group my-4">
+                <span
+                  className="input-group-text form-labels border-r-0 rounded-lg border-0 shadow"
+                  id="basic-addon1"
+                >
+                  <i className="bi bi-person-workspace text-2xl text-white"></i>
+                </span>
+                <input
+                  type="text"
+                  className={`form-control border-l-0 rounded-lg border-0 shadow text-${
+                    isAdmnin ? "green-600" : "red-600"
+                  } font-semibold`}
+                  placeholder="Admin"
+                  aria-label="Username"
+                  value={isAdmnin ? "True" : "False"}
+                  aria-describedby="basic-addon1"
+                  required
+                />
+                <span
+                  className="input-group-text form-labels border-l-0 rounded-lg border-0 shadow"
+                  onClick={async () => {
+                    await setIsAdmin(!isAdmnin);
+                    setForm({ ...form, administrator: isAdmnin });
+                    console.log(form);
+                  }}
+                  htmlFor="admin-checkbox"
+                >
+                  <i
+                    className={`bi ${
+                      isAdmnin ? "bi-check-lg" : "bi-x-lg"
+                    } text-lg text-white`}
+                  ></i>
+                </span>
+                {/* <input
+                  type="checkbox"
+                  id="admin-checkbox"
+                  hidden
+                  name="administrator"
+                  onChange={handleChange}
+                  value={form.administrator}
+                /> */}
+              </div>
+              {/* PASSWORD.  */}
+              <div className="input-group my-4">
+                <span
+                  className="input-group-text form-labels border-r-0 rounded-lg border-0 shadow"
+                  id="basic-addon1"
+                >
+                  <i className="bi bi-key-fill text-xl text-white"></i>
+                </span>
+                <input
+                  type={passwordVisibility ? "text" : "password"}
+                  className="form-control border-l-0 rounded-lg border-0 shadow"
+                  placeholder="Password"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                  onChange={handleChange}
+                  value={form.password}
+                  name="password"
+                  required
+                />
+                <span
+                  className="input-group-text form-labels border-l-0 rounded-lg border-0 shadow"
+                  onClick={() => {
+                    setPasswordVisibility(!passwordVisibility);
+                  }}
+                >
+                  <i
+                    className={`bi ${
+                      passwordVisibility ? "bi-eye-slash" : "bi-eye-fill"
+                    } text-lg text-white`}
+                  ></i>
+                </span>
+              </div>
+              {/* CONFIRM PASSWORD.  */}
+              <div className="input-group my-4">
+                <span
+                  className="input-group-text form-labels border-r-0 rounded-lg border-0 shadow"
+                  id="basic-addon1"
+                >
+                  <i className="bi bi-key-fill text-2xl text-white"></i>
+                </span>
+                <input
+                  type={confirmPasswordVisibility ? "text" : "password"}
+                  className="form-control border-l-0 rounded-lg border-0 shadow"
+                  placeholder="Confirm Password"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                  name="confirmPassword"
+                  onChange={handleChange}
+                  value={form.confirmPassword}
+                  required
+                />
+                <span
+                  className="input-group-text form-labels border-l-0 rounded-lg border-0 shadow"
+                  onClick={() => {
+                    setConfirmPasswordVisibility(!confirmPasswordVisibility);
+                  }}
+                >
+                  <i
+                    className={`bi ${
+                      confirmPasswordVisibility ? "bi-eye-slash" : "bi-eye-fill"
+                    } text-lg text-white`}
+                  ></i>
+                </span>
+              </div>
+              {/* TERMS & CONDITIONS  */}
+              <div className="form-check flex justify-center items-center mt-4">
+                <input
+                  className="form-check-input mr-2 terms-checkbox"
+                  type="checkbox"
+                  value=""
+                  id="flexCheckDefault"
+                />
+                <label
+                  className="form-check-label text-xs mt-1"
+                  htmlFor="flexCheckDefault"
+                >
+                  I have read and agreed to{" "}
+                  <span className="text-themeViolet1 cursor-pointer">
+                    Terms & Conditions
+                  </span>
+                </label>
+              </div>
+              {/* SIGN UP BUTTON  */}
+              <div className="flex justify-center mt-4">
+                <button
+                  className="rounded-lg py-2 px-8 landing-review text-white form-labels hover:scale-x-110 transition-all"
+                  type="submit"
+                >
+                  CREATE ACCOUNT
+                </button>
+              </div>
+              {/* ALREADY HAVE AN ACCOUNT  */}
+              <div className="form-check flex justify-center items-center my-4 p-0">
+                <label
+                  className="form-check-label text-xs mt-1"
+                  htmlFor="flexCheckDefault"
+                >
+                  Already have an account?{" "}
+                  <span className="text-lightBlue1 cursor-pointer">
+                    Sign In
+                  </span>
+                </label>
+              </div>
+            </form>
           </div>
         </div>
       </div>
