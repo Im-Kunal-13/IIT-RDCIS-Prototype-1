64477fdaed937965164db6c15d5183380cafc87c
ImKunal13z <2002kunalmondal13@gmail.com>
2022-04-27 04:49:33 +0530

    Recaptcha before deleting user implemented

--- frontend/src/Pages/Dashboard/sub-components/DeleteUserModal.jsx
+++ frontend/src/Pages/Dashboard/sub-components/DeleteUserModal.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteUser } from "../../../features/auth/authSlice";
 import { toast } from "react-toastify";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export default function DeleteUserModal({ user, index }) {
   const dispatch = useDispatch();
+
+  // Getting reference to the hidden close button.
+  const closeBtn = useRef(null);
+
+  // Getting reference to the hidden close button.
+  const reCaptchaBtn = useRef(null);
+
+  // Recaptcha state
+  const [reCaptchaState, setReCaptchaState] = useState(false);
+
+  // Recaptcha handling
+  const onRecaptchaChange = (value) => {
+    console.log("Captcha value:", value);
+
+    // Setting the recaptcha state value to true.
+    if (value) {
+      setReCaptchaState(true);
+    }
+  };
 
   // Destructuring data.
   const {
@@ -15,7 +35,15 @@ export default function DeleteUserModal({ user, index }) {
   } = useSelector((state) => state.auth);
 
   const onDelete = async () => {
-    await dispatch(deleteUser(user._id));
+    if (!reCaptchaState) {
+      toast.info("Fill recaptcha to proceed.", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+        toastId: "logoutSucces1",
+      });
+    } else {
+      closeBtn.current.click();
+      await dispatch(deleteUser(user._id));
+    }
   };
   return (
     <div
@@ -49,22 +77,35 @@ export default function DeleteUserModal({ user, index }) {
               aria-label="Close"
             ></i>
           </div>
-          <div className="modal-body">
-            Are you sure you want to delete this user?
+          <div className="modal-body px-0">
+            <span className="mb-2 px-3">
+              Are you sure you want to delete this user?
+            </span>
+            <ReCAPTCHA
+              sitekey="6Le2JKQfAAAAAE8E9w1MCFRKvUttuKn0xvmIt71C"
+              onChange={onRecaptchaChange}
+              size="normal"
+              className="px-3 mt-3"
+              ref={reCaptchaBtn}
+              defaultValue={null}
+            />
           </div>
-          <div className="modal-footer">
+          <div className="modal-footer px-0">
             <button
               type="button"
               className="text-white border px-4 py-1 rounded-sm bg-lightBlue2 shadow hover:scale-x-110 transition-all"
-              data-bs-dismiss="modal"
               onClick={onDelete}
             >
+              Yes
+            </button>
+            <button type="button" data-bs-dismiss="modal" hidden>
               Yes
             </button>
             <button
               type="button"
               className="border px-4 py-1 text-lightBlue2 rounded-sm shadow hover:scale-x-110 transition-all"
               data-bs-dismiss="modal"
+              ref={closeBtn}
             >
               No
             </button>
