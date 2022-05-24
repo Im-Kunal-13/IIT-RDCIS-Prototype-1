879d6939c7524c9178cff913877271f09b3ad25c
ImKunal13z <2002kunalmondal13@gmail.com>
2022-05-24 10:39:46 +0530

    back to react 18

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -11,12 +12,13 @@ const container = document.getElementById("root");
 // Create a root.
 const root = ReactDOMClient.createRoot(container);
 
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
